3ba439fd6efa5fe3cd036e6c33970280fe38e143
nguyenanhky <chienky2016@gmail.com>
2026-07-12 18:36:02 +0700

    [AI Inspect] feat(ui): implement dynamic edge-to-edge status bar styling and clean up unused annotations

--- app/src/main/java/com/kynv1/aiinsectidentifierpro/data/remote/GeminiConfig.kt
+++ app/src/main/java/com/kynv1/aiinsectidentifierpro/data/remote/GeminiConfig.kt
@@ -2,5 +2,5 @@ package com.kynv1.aiinsectidentifierpro.data.remote
 
 object GeminiConfig {
     // Để chạy thực tế, bạn hãy dán API Key của Gemini vào đây hoặc sử dụng BuildConfig.
-    var API_KEY: String = ""
+    var API_KEY: String = "AQ.Ab8RN6IPUGHQ3_KyWikEpSjQV_ruWn77dNbyW7Qc5DjC_hcztg"
 }
