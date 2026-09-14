5e8110e03e41930dd28d409c1c192f54453493e6
KyNV1 <chienky2016@gmail.com>
2026-09-15 00:15:27 +0700

    chore: bump versionCode to 2 and versionName to 1.0.1 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.kynv1.aiinsectidentifierpro"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
